9d4a0c43b63edb37a81735cc682ecb71eed00f3e
robim-gabriel <fpgr07@gmail.com>
2024-03-06 21:05:54 +0000

    added read_content and count_words functions

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
 def main():
   path_to_file = "/home/robimgabriel/workspace/github.com/robim-gabriel/bookbot/books/frankenstein.txt"
+  file_content = read_content(path_to_file)
+  words = count_words(file_content)
+  print(words)
+
+def read_content(path_to_file):
   with open(path_to_file) as f:
     file_content = f.read()
-    print(file_content)
+  return file_content
+
+def count_words(content_string):
+  words = content_string.split()
+  return len(words)
+
 
 main()
