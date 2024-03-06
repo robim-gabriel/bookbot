d3f95c79bf1d70b4a28032d840b2c954e51fa1e7
robim-gabriel <fpgr07@gmail.com>
2024-03-06 21:23:19 +0000

    Added function count_each_letter

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 def main():
   path_to_file = "/home/robimgabriel/workspace/github.com/robim-gabriel/bookbot/books/frankenstein.txt"
   file_content = read_content(path_to_file)
-  words = count_words(file_content)
-  print(words)
+  num_words = count_words(file_content)
+  chars = count_each_letter(file_content)
 
 def read_content(path_to_file):
   with open(path_to_file) as f:
@@ -13,5 +13,12 @@ def count_words(content_string):
   words = content_string.split()
   return len(words)
 
+def count_each_letter(content_string):
+  letters_dict = {letter: 0 for letter in "abcdefghijklmnopqrstuvwxyz"}
+  lowered_string = content_string.lower()
+  for letter in lowered_string:
+    if letter in letters_dict:
+      letters_dict[letter] += 1
+  return letters_dict
 
 main()
